f84c6af6b302233d2a4948d30e374e5e4366d8f0
MrRYUHS <96356237+MrRYUHS@users.noreply.github.com>
2023-02-26 22:46:35 +0900

    보드게시판 바로가기

--- ch2/src/main/java/com/fastcampus/ch2/PerformanceFilter.java
+++ ch2/src/main/java/com/fastcampus/ch2/PerformanceFilter.java
@@ -29,7 +29,10 @@ public class PerformanceFilter implements Filter {
 		chain.doFilter(request, response); 
 		
 		// 3. 후처리 작업
-		System.out.print("["+((HttpServletRequest)request).getRequestURI()+"]");
+		HttpServletRequest req = (HttpServletRequest)request;
+		String referer = req.getHeader("referer");
+		String method = req.getMethod();
+		System.out.print("["+referer+"] -> "+ method+ "["+req.getRequestURI()+"]");
 		System.out.println(" 소요시간="+(System.currentTimeMillis()-startTime)+"ms");
 	}
 
@@ -37,5 +40,4 @@ public class PerformanceFilter implements Filter {
 	public void destroy() {
 		// 정리 작업
 	}
-
 }
