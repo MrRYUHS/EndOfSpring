2e7e27dcd226697d64a7f6b2e85f6fbaf6caa640
MrRYUHS <96356237+MrRYUHS@users.noreply.github.com>
2023-03-09 23:57:14 +0900

    ch2 28

--- ch2/src/main/java/com/fastcampus/ch2/ExceptionController.java
+++ ch2/src/main/java/com/fastcampus/ch2/ExceptionController.java
@@ -2,10 +2,12 @@ package com.fastcampus.ch2;
 
 import java.io.FileNotFoundException;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class ExceptionController {
@@ -15,8 +17,12 @@ public class ExceptionController {
 		return "error";
 	}
 	@ExceptionHandler(Exception.class)
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR) // 200 -> 500
 	public String catcher(Exception ex, Model m) {
+		System.out.println("catcher() in ExceptionController");
+		System.out.println("m="+m);
 		m.addAttribute("ex", ex);
+		
 		return "error";
 	}
 	@RequestMapping("/ex")
